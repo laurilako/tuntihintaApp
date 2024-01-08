30cf23d2944d0f7a4c8d5cc72bdbfec4e529c3f4
laurilako <konsta.laurila1@gmail.com>
2024-01-08 18:18:41 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Tuntihinta</h1>
+      <div className="card">
+        <h2>👋</h2>
+        <p>
+          placeholder
+        </p>
+      </div>
+      <p className="note">
+        Data from ...
+      </p>
+    </>
+  )
+}
+
+export default App
